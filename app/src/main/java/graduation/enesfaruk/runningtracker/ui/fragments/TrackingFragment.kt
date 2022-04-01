2ae66b359e4cdc7713b60e0822b27763b7318129
EnesFaruk1 <enes.isik@appcent.mobi>
2022-04-01 15:19:38 +0300

    -Google Maps connection

--- app/src/main/java/graduation/enesfaruk/runningtracker/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/graduation/enesfaruk/runningtracker/ui/fragments/TrackingFragment.kt
@@ -1,10 +1,14 @@
 package graduation.enesfaruk.runningtracker.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
 import graduation.enesfaruk.runningtracker.R
 import graduation.enesfaruk.runningtracker.ui.viewmodels.MainViewModel
+import kotlinx.android.synthetic.main.fragment_tracking.*
 
 /**
  * Created by Enes Faruk Işık on 26.03.2022.
@@ -15,4 +19,44 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private val viewModel: MainViewModel by viewModels()
 
+    private var map: GoogleMap? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView.onCreate(savedInstanceState)
+
+        mapView.getMapAsync{
+            map = it
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+    }
 }
